e70e27e31995056276c02a2855ef2337bf1fa7be
strongfanfan <463428221@qq.com>
2017-02-24 16:26:53 +0800

    箭头函数arg

--- README.md
+++ README.md
@@ -30,3 +30,21 @@
         //‘+’是将type转换为Number
       //length === +length等价于typeof length === “number” && !isNaN(length)
 ```
+```js
+ const ssss = () => {
+       return Array.prototype.slice.call(arguments,0)
+    }
+    console.log(ssss(1,2,3,45,456)) 
+    //arguments is undefined
+```
+> 箭头函数不会在其内部暴露出  arguments 对象： arguments.length, arguments[0], arguments[1] 等等，都不会指向箭头函数的 arguments，而是指向了箭头函数所在作用域的一个名为 arguments 的值（如果有的话，否则，就是 undefined。——译者注）。
+
+使用rest参数
+
+```js
+const ssss = (...arg) => {
+       return Array.prototype.slice.call(arg,0)
+    }
+    console.log(ssss(1,2,3,45,456)) 
+    // 1,2,3,45,456
+```
--- test.js
+++ test.js
@@ -1,18 +1,41 @@
-var ddd = (n) => {
-            if (n == 1) return 1;
-            if (n == 0) return 0;
-            return ddd(n - 1) + ddd(n - 2)
-        };
-let a=1,
-    f1=()=> a=ddd(42);
-    f2=()=> console.log(a);
-    const f3 = (fn,delay) => {
-        setTimeout(function(){
-            a=ddd(43);
-            fn()
-        },delay)
-    }
-    f3(f2,300)
-    f3(f2,100)
-    setTimeout(f2,200);
-    f2()
+// var ddd = (n) => {
+//             if (n == 1) return 1;
+//             if (n == 0) return 0;
+//             return ddd(n - 1) + ddd(n - 2)
+//         };
+// let a=1,
+//     f1=()=> a=ddd(42);
+//     f2=()=> console.log(a);
+//     const f3 = (fn,delay) => {
+//         setTimeout(function(){
+//             a=ddd(43);
+//             fn()
+//         },delay)
+//     }
+//     f3(f2,300)
+//     f3(f2,100)
+//     setTimeout(f2,200);
+//     f2()
+
+
+
+    // const ssss = (...arg) => {
+    //    return Array.prototype.slice.call(arg,0)
+    // }
+    // console.log(ssss(1,2,3,45,456)) 
+    // // //TypeError: arguments.slice is not a function
+    
+    //    function ssss() {
+    //         return Array.prototype.slice.call(arguments, 0)
+    //     }
+    //     console.log(ssss(1, 2, 3, 45, 456))
+
+
+
+var arr = [1,2,3,4],
+sum = 0;
+sum1=0
+console.log(arr.map(function(obj){sum += obj}))
+console.log(sum);
+console.log(arr.forEach(function(ds){sum1 += ds}));
+console.log(sum1);
