65ba70f03188c820dfcbdbd404ecf10037de2b91
strongfanfan <463428221@qq.com>
2017-03-15 15:11:34 +0800

    原型链set机制

--- test.js
+++ test.js
@@ -142,3 +142,24 @@ objCopy.name='ddd';
 objCopy.family.father='baba'
 console.log(obj);
 //test { name: 'sss', family: { father: 'papa', mother: 'mama' } }
+
+
+/**
+ *  
+ */
+ const obj={
+     a:1
+ },obj1={
+     obj:obj
+ },obj2=Object.create(obj1);
+ obj1.a=3;
+ obj2.obj.a=2
+ console.log(obj1);
+ /**1. 如果在 [[Prototype]] 链上层存在名为 foo 的普通数据访问属性并且没 有被标记为只读（writable:false） ，那就会直接在 myObject 中添加一个名为 foo 的新 属性，它是屏蔽属性。 
+  * 
+  
+  2. 如果在 [[Prototype]] 链上层存在 foo，但是它被标记为只读（writable:false） ，那么 无法修改已有属性或者在 myObject 上创建屏蔽属性。如果运行在严格模式下，代码会 抛出一个错误。否则，这条赋值语句会被忽略。总之，不会发生屏蔽。 
+  * 3. 如果在 [[Prototype]] 链上层存在 foo 并且它是一个 setter（参见第 3 章） ，那就一定会 调用这个 setter。foo 不会被添加到（或者说屏蔽于）myObject，也不会重新定义 foo 这 个 setter。 
+  * 
+  */
+
