fa90884cc78e413c0c5f91ca767eababc45b9f1c
strongfanfan <463428221@qq.com>
2017-03-17 16:16:11 +0800

    toString

--- README.md
+++ README.md
@@ -22,7 +22,7 @@
         //两个都为真，且值由后面的变量决定
 
 ```
-
+---
 ```js
         if (length===+length){}
 
@@ -48,3 +48,54 @@ const ssss = (...arg) => {
     console.log(ssss(1,2,3,45,456)) 
     // 1,2,3,45,456
 ```
+------
+```js
+let a='hello',b='中文测试',c='!@#$%^&*(()';
+// console.log( Array.prototype.reverse.call(a))
+console.log(a.split('').reverse().join(''));
+console.log(b.split('').reverse().join(''));
+console.log(c.split('').reverse().join(''));
+
+
+//?你不知道的Js 15页的提示：上述方法对于包含复杂字符（Unicode，如星号、多字节字符等）的 字符串并不适用
+//好像可以用啊？？？
+
+```
+
+-------
+
+```js
+ const obj={
+     a:1
+ },obj1={
+     obj:obj
+ },obj2=Object.create(obj1);
+ obj1.a=3;
+ obj2.obj.a=2
+ console.log(obj1);
+ ```
+1. 如果在 [[Prototype]] 链上层存在名为 foo 的普通数据访问属性并且没 有被标记为只读（writable:false） ，那就会直接在 myObject 中添加一个名为 foo 的新 属性，它是屏蔽属性。 
+2. 如果在 [[Prototype]] 链上层存在 foo，但是它被标记为只读（writable:false） ，那么 无法修改已有属性或者在 myObject 上创建屏蔽属性。如果运行在严格模式下，代码会 抛出一个错误。否则，这条赋值语句会被忽略。总之，不会发生屏蔽。 
+3. 如果在 [[Prototype]] 链上层存在 foo 并且它是一个 setter（参见第 3 章） ，那就一定会 调用这个 setter。foo 不会被添加到（或者说屏蔽于）myObject，也不会重新定义 foo 这 个 setter。
+
+---
+
+```js
+let a = null, b = undefined, c = false, d = 7889, e = { a: 12 }, f = [1, 2, 3], g = a => a,
+    h = function () {
+        let a = 1
+    };
+// console.log(a.toString());//TypeError
+// console.log(b.toString());//TypeError
+console.log(c.toString());//false
+console.log(d.toString());//7889
+console.log(e.toString());//[object Object]
+console.log(f.toString());//1,2,3
+console.log(g.toString());//a => a
+console.log(h.toString());//function () {let a = 1}
+```
+1. 基本类型toString()后是字面量；
+2. 普通对象返回的是内部属性`[[class]]`的值
+1. 数组的toString方法经过重新定义，将所有单元字符串化以后再用 "," 连接起来
+1. 函数会返回函数体
+
--- test.js
+++ test.js
@@ -19,16 +19,16 @@
 
 
 
-    // const ssss = (...arg) => {
-    //    return Array.prototype.slice.call(arg,0)
-    // }
-    // console.log(ssss(1,2,3,45,456)) 
-    // // //TypeError: arguments.slice is not a function
-    
-    //    function ssss() {
-    //         return Array.prototype.slice.call(arguments, 0)
-    //     }
-    //     console.log(ssss(1, 2, 3, 45, 456))
+// const ssss = (...arg) => {
+//    return Array.prototype.slice.call(arg,0)
+// }
+// console.log(ssss(1,2,3,45,456)) 
+// // //TypeError: arguments.slice is not a function
+
+//    function ssss() {
+//         return Array.prototype.slice.call(arguments, 0)
+//     }
+//     console.log(ssss(1, 2, 3, 45, 456))
 
 
 
@@ -52,19 +52,19 @@
  */
 
 
-let arr=[1,2,3,4];
-function test (name) {
-    this.name=name;
-    this.family={
-        father:'papa',
-        mother:'mama'
+let arr = [1, 2, 3, 4];
+function test(name) {
+    this.name = name;
+    this.family = {
+        father: 'papa',
+        mother: 'mama'
     }
 }
-test.prototype.age=19;
-let obj= new test('ss');
+test.prototype.age = 19;
+let obj = new test('ss');
 // obj.__proto__.age='proto';
 // console.log(obj.__proto__);
-for (key in obj){
+for (key in obj) {
     console.log(obj[key]);
 }
 
@@ -73,23 +73,23 @@ for (key in obj){
 
 const shallowCopy = (source) => {
     let temp;
-    if(!source || typeof souce !== 'object'){
+    if (!source || typeof souce !== 'object') {
         return
     };
-    source instanceof Array?temp=[]:temp={};
-    for(key in source){
-        if(souce.hasOwnProperty(key)){
-            temp[key]=souce[key]
+    source instanceof Array ? temp = [] : temp = {};
+    for (key in source) {
+        if (souce.hasOwnProperty(key)) {
+            temp[key] = souce[key]
         }
     }
     return temp
 }
 
-let objCopy=shallowCopy(obj)
-console.log(objCopy,objCopy.age);
+let objCopy = shallowCopy(obj)
+console.log(objCopy, objCopy.age);
 //{ name: 'ss', family: { father: 'papa', mother: 'mama' } } undefined
-objCopy.name='c';
-objCopy.family.father='baba';
+objCopy.name = 'c';
+objCopy.family.father = 'baba';
 console.log(obj);
 //test { name: 'ss', family: { father: 'baba', mother: 'mama' } }
 
@@ -102,44 +102,44 @@ console.log(obj);
  */
 
 
-function test (name) {
-    this.name=name;
-    this.family={
-        father:'papa',
-        mother:'mama'
+function test(name) {
+    this.name = name;
+    this.family = {
+        father: 'papa',
+        mother: 'mama'
     }
 }
-test.prototype.age=19;
+test.prototype.age = 19;
 
 
 
 const deepCopy = (source) => {
     let temp;
-    if(!source || typeof source !== 'object'){
+    if (!source || typeof source !== 'object') {
         return
     };
-    source instanceof Array?temp=[]:temp={};
-    for(key in source){
-        if(source.hasOwnProperty(key)){
-            if(typeof source[key]==='object'){
-                temp[key]=deepCopy(source[key])
-            }else{
-                temp[key]=source[key]
+    source instanceof Array ? temp = [] : temp = {};
+    for (key in source) {
+        if (source.hasOwnProperty(key)) {
+            if (typeof source[key] === 'object') {
+                temp[key] = deepCopy(source[key])
+            } else {
+                temp[key] = source[key]
             }
         }
     }
     return temp
 }
-let obj=new test('sss'),arr=[
-    1,2,3,[
-        7,8,9
+let obj = new test('sss'), arr = [
+    1, 2, 3, [
+        7, 8, 9
     ]
 ]
-objCopy=deepCopy(obj)
+objCopy = deepCopy(obj)
 console.log(objCopy);
 //{ name: 'sss', family: { father: 'papa', mother: 'mama' } }
-objCopy.name='ddd';
-objCopy.family.father='baba'
+objCopy.name = 'ddd';
+objCopy.family.father = 'baba'
 console.log(obj);
 //test { name: 'sss', family: { father: 'papa', mother: 'mama' } }
 
@@ -147,21 +147,21 @@ console.log(obj);
 /**
  *  
  */
- const obj={
-     a:1
- },obj1={
-     obj:obj
- },obj2=Object.create(obj1);
- obj1.a=3;
- obj2.obj.a=2
- console.log(obj1);
- /**1. 如果在 [[Prototype]] 链上层存在名为 foo 的普通数据访问属性并且没 有被标记为只读（writable:false） ，那就会直接在 myObject 中添加一个名为 foo 的新 属性，它是屏蔽属性。 
-  * 
-  
-  2. 如果在 [[Prototype]] 链上层存在 foo，但是它被标记为只读（writable:false） ，那么 无法修改已有属性或者在 myObject 上创建屏蔽属性。如果运行在严格模式下，代码会 抛出一个错误。否则，这条赋值语句会被忽略。总之，不会发生屏蔽。 
-  * 3. 如果在 [[Prototype]] 链上层存在 foo 并且它是一个 setter（参见第 3 章） ，那就一定会 调用这个 setter。foo 不会被添加到（或者说屏蔽于）myObject，也不会重新定义 foo 这 个 setter。 
-  * 
-  */
+const obj = {
+    a: 1
+}, obj1 = {
+    obj: obj
+}, obj2 = Object.create(obj1);
+obj1.a = 3;
+obj2.obj.a = 2
+console.log(obj1);
+/**1. 如果在 [[Prototype]] 链上层存在名为 foo 的普通数据访问属性并且没 有被标记为只读（writable:false） ，那就会直接在 myObject 中添加一个名为 foo 的新 属性，它是屏蔽属性。 
+ * 
+ 
+ 2. 如果在 [[Prototype]] 链上层存在 foo，但是它被标记为只读（writable:false） ，那么 无法修改已有属性或者在 myObject 上创建屏蔽属性。如果运行在严格模式下，代码会 抛出一个错误。否则，这条赋值语句会被忽略。总之，不会发生屏蔽。 
+ * 3. 如果在 [[Prototype]] 链上层存在 foo 并且它是一个 setter（参见第 3 章） ，那就一定会 调用这个 setter。foo 不会被添加到（或者说屏蔽于）myObject，也不会重新定义 foo 这 个 setter。 
+ * 
+ */
 
 
 /**
@@ -169,30 +169,30 @@ console.log(obj);
  */
 
 
-  const obj={
-      set a (val){
-        this._a_=val
-      },
-      get a(){
-          return this._b_
-      },
-      set b(val){
-          this._b_=val
-      }
-  }
-  obj.a=1;
-  obj.b=2
-  console.log(obj.a);
+const obj = {
+    set a(val) {
+        this._a_ = val
+    },
+    get a() {
+        return this._b_
+    },
+    set b(val) {
+        this._b_ = val
+    }
+}
+obj.a = 1;
+obj.b = 2
+console.log(obj.a);
 
 
 /**
  * 
  */
 
-const obj={
-    log:()=>console.log('obj')
-},obj1={
-    log:()=>{
+const obj = {
+    log: () => console.log('obj')
+}, obj1 = {
+    log: () => {
         obj.log.call(this)
     }
 }
@@ -203,24 +203,24 @@ const obj={
 
 // es6  箭头函数 argument对象缺失  使用剩余参数
 
-const test =(...args)=>{
-    console.log(args[0],args[1]);
+const test = (...args) => {
+    console.log(args[0], args[1]);
 }
- 
- test('sd','er')
 
- function test1(){
-     console.log( arguments[0],arguments[1])
- }
-  test1('sd','er')
+test('sd', 'er')
+
+function test1() {
+    console.log(arguments[0], arguments[1])
+}
+test1('sd', 'er')
 
 
 //+ 的强制类型转换
-let d='sd',s= + d;
+let d = 'sd', s = + d;
 console.log(typeof s)
 
 
-let a='hello',b='中文测试',c='!@#$%^&*(()';
+let a = 'hello', b = '中文测试', c = '!@#$%^&*(()';
 // console.log( Array.prototype.reverse.call(a))
 console.log(a.split('').reverse().join(''));
 console.log(b.split('').reverse().join(''));
@@ -235,16 +235,16 @@ console.log(c.split('').reverse().join(''));
  * 
  */
 
-var person = function(name){
+var person = function (name) {
     this.name = name
 };
 
-person.prototype.getName = function(){
-    return this.name; 
+person.prototype.getName = function () {
+    return this.name;
 }
 
 var zzz = new person('zzz');
-console.log(zzz.getName()); 
+console.log(zzz.getName());
 
 console.log(zzz.__proto__ == person.prototype);// true
 console.log(person.__proto__ == Function.prototype);// true
@@ -254,4 +254,23 @@ console.log(Object.__proto__);// function
 console.log(Object.prototype.__proto__);// null 
 console.log(Function.__proto__);// function 
 console.log(Function.prototype.__proto__) //object
+
+/**
+ * 
+ */
+
+let a = null, b = undefined, c = false, d = 7889, e = { a: 12 }, f = [1, 2, 3], g = a => a,
+    h = function () {
+        let a = 1
+    };
+// console.log(a.toString());//TypeError
+// console.log(b.toString());//TypeError
+console.log(c.toString());
+console.log(d.toString());
+console.log(e.toString());
+console.log(f.toString());
+console.log(g.toString());
+console.log(h.toString());
+
+
 
