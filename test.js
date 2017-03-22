7040ee57e1766b689c5a11bdd3c20b3d56505b46
strongfanfan <463428221@qq.com>
2017-03-22 10:32:49 +0800

    数据处理，map格式tree格式的数据共享

--- test.js
+++ test.js
@@ -290,3 +290,56 @@ test({
 ///参数必须和解构对象属性重名
 
 
+/**
+ * 
+ */
+//知乎民工叔  数据共享 讲解  pocket 标签 immu
+
+const regions = [
+  {id: 1, name: 'Jiangsu'},
+  {id: 2, name: 'Zhejiang'},
+  {id: 3, name: 'Yunnan'},
+  {id: 11, name: 'Nanjing', parent: 1},
+  {id: 12, name: 'Suzhou', parent: 1},
+  {id: 21, name: 'Hangzhou', parent: 2},
+  {id: 22, name: 'Ningbo', parent: 2},
+  {id: 31, name: 'Kunming', parent: 3},
+  {id: 32, name: 'Lijiang', parent: 3},
+  {id: 111, name: 'Jiangning', parent: 11},
+  {id: 112, name: 'Gaochun', parent: 11},
+  {id: 321, name: 'Ninglang', parent: 32},
+  {id: 322, name: 'Huaping', parent: 32}
+]
+
+const getTree = arr => {
+  const map = regions.reduce((acc, val) => {
+    acc[val.id] = val
+    return acc
+  }, {})
+
+  const tree = []
+  arr.forEach(region => {
+    if (region.parent) {
+      const parent = map[region.parent]
+      if (!parent.children) { 
+        parent.children = [region]
+      }
+      else {
+        parent.children.push(region)
+      }
+    }
+    else {
+      tree.push(region)
+    }
+  })
+
+  return { map, tree }
+}
+
+let a=getTree(regions)
+console.log(JSON.stringify(a,null,3));
+
+///
+/**
+ * 
+ */
